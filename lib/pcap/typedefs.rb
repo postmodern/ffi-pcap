86dcef6095c463d6653c382fb68e79beb86a8e00
postmodern <postmodern.mod3@gmail.com>
2009-05-21 15:19:18 -0700

    Added a SockAddr struct.

--- lib/pcap/typedefs.rb
+++ lib/pcap/typedefs.rb
@@ -7,6 +7,7 @@ module FFI
 
   alias_type :long, :time_t
   alias_type :long, :suseconds_t
+  alias_type :ushort, :sa_family_t
   alias_type :int, :bpf_int32
   alias_type :uint, :bpf_uint32
   alias_type :int, :pcap_direction_t
