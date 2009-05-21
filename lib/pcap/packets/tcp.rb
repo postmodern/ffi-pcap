aa82f235d201b0107753bc8ff04560491b9f3f6c
postmodern <postmodern.mod3@gmail.com>
2009-05-21 16:22:30 -0700

    Added comments.

--- lib/pcap/packets/tcp.rb
+++ lib/pcap/packets/tcp.rb
@@ -7,15 +7,33 @@ module FFI
     module Packets
       class TCP < FFI::Struct
 
+        # TCP flags
         FLAGS = [
+          # Final packet flag
           FIN = 0x01,
+
+          # Synchronization packet flag
           SYN = 0x02,
+
+          # Reset packet flag
           RST = 0x04,
+
+          # Push packet flag
           PUSH = 0x08,
+
+          # Acknowledgement packet flag
           ACK = 0x10,
+
+          # Urgent data packet flag
           URG = 0x20,
+
+          # ECE packet flag
           ECE = 0x40,
+
+          # CWR packet flag
           CWR = 0x80,
+
+          # All combined flags
           XMAS = (FIN | SYN | RST | PUSH | ACK | URG | ECE | CWR)
         ]
 
@@ -29,58 +47,101 @@ module FFI
                :th_sum, :ushort,
                :th_urp, :ushort
 
+        #
+        # Returns the source port.
+        #
         def src_port
           self[:th_sport]
         end
 
+        #
+        # Returns the destination port.
+        #
         def dest_port
           self[:th_dport]
         end
 
+        #
+        # Returns the sequence number of the packet.
+        #
         def seq
           self[:th_seq]
         end
 
+        #
+        # Returns the acknowledgement number of the packet.
+        #
         def ack
           self[:th_ack]
         end
 
-        def acked?(num)
-          self[:th_ack] == num
-        end
-
+        #
+        # Returns the data offset for the packet.
+        #
         def offset
           (self[:th_offx2] & 0xf0) >> 4
         end
 
+        #
+        # Returns +true+ if the packet has the FIN flag set, returns
+        # +false+ otherwise.
+        #
         def fin?
           (self[:th_flags] & FIN) != 0
         end
 
+        #
+        # Returns +true+ if the packet has the SYN flag set, returns
+        # +false+ otherwise.
+        #
         def syn?
           (self[:th_flags] & SYN) != 0
         end
 
+        #
+        # Returns +true+ if the packet has the RST flag set, returns
+        # +false+ otherwise.
+        #
         def rst?
           (self[:th_flags] & RST) != 0
         end
 
+        #
+        # Returns +true+ if the packet has the PUSH flag set, returns
+        # +false+ otherwise.
+        #
         def push?
           (self[:th_flags] & PUSH) != 0
         end
 
+        #
+        # Returns +true+ if the packet has the ACK flag set, returns
+        # +false+ otherwise.
+        #
         def ack?
           (self[:th_flags] & ACK) != 0
         end
 
+        #
+        # Returns +true+ if the packet has the URG flag set, returns
+        # +false+ otherwise.
+        #
         def urg?
           (self[:th_flags] & URG) != 0
         end
 
+        #
+        # Returns +true+ if the packet has the ECE flag set, returns
+        # +false+ otherwise.
+        #
         def ece?
           (self[:th_flags] & ECE) != 0
         end
 
+        #
+        # Returns +true+ if the packet has the CWR flag set, returns
+        # +false+ otherwise.
+        #
         def cwr?
           (self[:th_flags] & CWR) != 0
         end
