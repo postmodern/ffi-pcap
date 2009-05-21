104639110d661b8fa3356019e8bbfa54873a661a
postmodern <postmodern.mod3@gmail.com>
2009-05-21 03:21:39 -0700

    Have PCap::Handler merely wrap around the pointer to pcap_t.
    * Added Handler.open_dead.
    * Added Handler.open_live.

--- lib/pcap/handler.rb
+++ lib/pcap/handler.rb
@@ -1,16 +1,54 @@
 require 'pcap/ffi'
+require 'pcap/data_link'
 
 require 'ffi'
 
 module FFI
   module PCap
-    class Handler < FFI::MemoryPointer
+    class Handler
 
       include Enumerable
 
+      # Default snaplen
+      SNAPLEN = 65535
+
+      # Pointer to the pcap opaque type
+      attr_reader :pcap
+
+      # Number of packets to sniff
       attr_accessor :count
 
-      def self.open_live(device,spanlen,promisc,to_ms)
+      def initialize(pcap)
+        @pcap = pcap
+      end
+
+      def self.open_live(options={})
+        device = options[:device]
+        promisc = if options[:promisc]
+                    1
+                  else
+                    0
+                  end
+        snaplen = (options[:snaplen] || SNAPLEN)
+        to_ms = (options[:timeout] || 0)
+
+        ptr = PCap.pcap_open_live(device,snaplen,promisc,to_ms,nil)
+
+        unless ptr
+          raise(StandardError,errbuf,caller)
+        end
+
+        return self.new(ptr)
+      end
+
+      def self.open_dead(datalink,snaplen=SNAPLEN)
+        datalink = DataLink[datalink]
+
+        return self.new(PCap.pcap_open_dead(datalink,snaplen))
+      end
+
+      def datalink
+        DataLink.new(PCap.pcap_datalink(@pcap))
       end
 
       def callback(&block)
@@ -22,13 +60,13 @@ module FFI
       end
 
       def direction=(dir)
-        PCap.pcap_setdirection(self,dir)
+        PCap.pcap_setdirection(@pcap,dir)
       end
 
       def loop(data,&block)
         callback(&block) if block
 
-        PCap.pcap_loop(self,@count,@callback,data)
+        PCap.pcap_loop(@pcap,@count,@callback,data)
       end
 
       alias :each :loop
@@ -36,22 +74,26 @@ module FFI
       def dispatch(data,&block)
         callback(&block) if block
 
-        PCap.pcap_dispatch(self,@count,@callback,data)
+        PCap.pcap_dispatch(@pcap,@count,@callback,data)
       end
 
       def next
         header = PacketHeader.new
-        data = PCap.pcap_next(self,header)
+        data = PCap.pcap_next(@pcap,header)
 
         return [header, data]
       end
 
       def stop
-        PCap.pcap_breakloop(self)
+        PCap.pcap_breakloop(@pcap)
       end
 
       def close
-        PCap.pcap_close(self)
+        PCap.pcap_close(@pcap)
+      end
+
+      def inspect
+        "#<#{self.class}: 0x#{@pcap.to_s(16)}>"
       end
 
     end
