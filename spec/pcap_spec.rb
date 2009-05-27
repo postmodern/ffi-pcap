a00df43a410a39982fbfe08467e4c0ca6308a726
postmodern <postmodern.mod3@gmail.com>
2009-05-26 19:01:40 -0700

    Added a spec for PCap.device.

--- spec/pcap_spec.rb
+++ spec/pcap_spec.rb
@@ -7,4 +7,11 @@ describe PCap do
   it "should define a VERSION constant" do
     PCap.const_defined?('VERSION').should == true
   end
+
+  it "should return the name of a device suitable for open_live" do
+    dev = PCap.device
+
+    dev.should_not be_nil
+    dev.should_not be_empty
+  end
 end
