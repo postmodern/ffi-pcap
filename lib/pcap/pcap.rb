aad76ec695d9be975160bc666e1a4ff73b07f233
postmodern <postmodern.mod3@gmail.com>
2009-05-26 17:58:02 -0700

    Properly handle the errbuf used by pcap_open_live and pcap_open_dead.
    * Added PCap.open_offline.

--- lib/pcap/pcap.rb
+++ lib/pcap/pcap.rb
@@ -13,11 +13,12 @@ module FFI
                 end
       snaplen = (options[:snaplen] || Handler::SNAPLEN)
       to_ms = (options[:timeout] || 0)
+      errbuf = MemoryPointer.new(ERRBUF_SIZE)
 
       ptr = PCap.pcap_open_live(device,snaplen,promisc,to_ms,nil)
 
       unless ptr
-        raise(StandardError,errbuf,caller)
+        raise(StandardError,errbuf.get_string(ERRBUF_SIZE),caller)
       end
 
       return Handler.new(ptr)
@@ -27,6 +28,19 @@ module FFI
       datalink = DataLink[datalink]
 
       return Handler.new(PCap.pcap_open_dead(datalink,snaplen))
+    end
+
+    def PCap.open_offline(path)
+      path = File.expand_path(path)
+      errbuf = MemoryPointer.new(ERRBUF_SIZE)
+
+      ptr = PCap.pcap_open_offline(path,errbuf)
+
+      unless ptr
+        raise(StandardError,errbuf.get_string(ERRBUF_SIZE),caller)
+      end
+
+      return Handler.new(ptr)
     end
   end
 end
