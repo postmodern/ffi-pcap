b003c7a33a0618f6d37ef825403dbc2b9794e9d9
postmodern <postmodern.mod3@gmail.com>
2009-05-26 19:06:20 -0700

    Make Dumper more like Handler in how it wraps around a pointer.

--- lib/pcap/dumper.rb
+++ lib/pcap/dumper.rb
@@ -6,19 +6,28 @@ module FFI
   module PCap
     class Dumper < FFI::MemoryPointer
 
-      def self.open(path)
+      def initialize(dumper)
+        @dumper = dumper
       end
 
       def tell
-        PCap.pcap_dump_ftell(self)
+        PCap.pcap_dump_ftell(@dumper)
       end
 
       def flush
-        PCap.pcap_dump_flush(self)
+        PCap.pcap_dump_flush(@dumper)
       end
 
       def close
-        PCap.pcap_dump_close(self)
+        PCap.pcap_dump_close(@dumper)
+      end
+
+      def to_ptr
+        @dumper
+      end
+
+      def inspect
+        "#<#{self.class}: 0x#{@dumper.to_s(16)}>"
       end
 
     end
