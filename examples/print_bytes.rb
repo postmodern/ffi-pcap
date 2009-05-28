f70e449ed536aa3bbb090f7eb88bd869799fd5fd
postmodern <postmodern.mod3@gmail.com>
2009-05-27 22:26:20 -0700

    Added an example which merely prints the bytes from the packets.

---
+++ examples/print_bytes.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'pcap'
+
+include FFI
+
+pcap = PCap.open_live(:device => ARGV[0]) do |user,header,bytes|
+  puts "#{header.timestamp}:"
+
+  header.captured.times { |i|
+    print ' %2x' % bytes.get_uchar(i)
+  }
+  putc "\n"
+end
+
+pcap.loop
