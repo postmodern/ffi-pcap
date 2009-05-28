bcae8cabf1085180b3d4039b213197139a055734
postmodern <postmodern.mod3@gmail.com>
2009-05-27 22:33:24 -0700

    Fixed hex formatting.

--- examples/print_bytes.rb
+++ examples/print_bytes.rb
@@ -9,7 +9,7 @@ pcap = PCap.open_live(:device => ARGV[0]) do |user,header,bytes|
   puts "#{header.timestamp}:"
 
   header.captured.times { |i|
-    print ' %2x' % bytes.get_uchar(i)
+    print ' %.2x' % bytes.get_uchar(i)
   }
   putc "\n"
 end
