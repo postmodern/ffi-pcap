a0b5317d4890cc7d54820b1938ced533eda2f00e
postmodern <postmodern.mod3@gmail.com>
2009-05-27 20:51:29 -0700

    Added some helper methods to the Stat struct.

--- lib/pcap/stat.rb
+++ lib/pcap/stat.rb
@@ -6,6 +6,18 @@ module FFI
       layout :ps_recv, :uint,
              :ps_drop, :uint,
              :ps_ifdrop, :uint
+
+      def received
+        self[:ps_recv]
+      end
+
+      def dropped
+        self[:ps_drop]
+      end
+
+      def interface_dropped
+        self[:ps_ifdrop]
+      end
     end
   end
 end
