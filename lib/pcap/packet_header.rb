03bab6bffd8cfe28e2e2931e4119b38cf519f92c
postmodern <postmodern.mod3@gmail.com>
2009-05-27 23:19:03 -0700

    Added some helper methods.

--- lib/pcap/packet_header.rb
+++ lib/pcap/packet_header.rb
@@ -9,6 +9,18 @@ module FFI
       layout :ts, TimeVal,
              :caplen, :bpf_uint32,
              :len, :bpf_uint32
+
+      def timestamp
+        self[:ts]
+      end
+
+      def captured
+        self[:caplen]
+      end
+
+      def length
+        self[:len]
+      end
     end
   end
 end
