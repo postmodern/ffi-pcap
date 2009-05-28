c5d1b9ffd3a0919b3431d2b021a3f6b2a325bad6
postmodern <postmodern.mod3@gmail.com>
2009-05-27 21:15:12 -0700

    Added specs for DataLink.

---
+++ spec/data_link_spec.rb
@@ -0,0 +1,25 @@
+require 'pcap/data_link'
+
+require 'spec_helper'
+
+describe DataLink do
+  before(:all) do
+    @datalink = DataLink.new(0)
+  end
+
+  it "should be initialized from a pcap datalink value" do
+    @datalink.name.should == 'NULL'
+  end
+
+  it "should map datalink names to pcap datalink values" do
+    DataLink[:en10mb].should == 1
+  end
+
+  it "should have a description" do
+    @datalink.description.should_not be_empty
+  end
+
+  it "should be able to convert to an Integer" do
+    @datalink.to_i.should == 0
+  end
+end
