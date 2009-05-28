0e42da699afdaa83e1d226972ee5da51f6ad2c11
postmodern <postmodern.mod3@gmail.com>
2009-05-27 23:30:37 -0700

    Added a rough set of specs for the different types of Handlers.

---
+++ spec/handler_spec.rb
@@ -0,0 +1,49 @@
+require 'pcap/handler'
+
+require 'spec_helper'
+require 'helpers/dumps'
+require 'handler_examples'
+
+describe PCap::Handler do
+  describe "offline" do
+    before(:all) do
+      @pcap = PCap.open_offline(dump_path('simple_tcp'))
+    end
+
+    after(:all) do
+      @pcap.close
+    end
+
+    it_should_behave_like "Handler"
+  end
+
+  describe "live" do
+    before(:each) do
+      @pcap = PCap.open_live
+      @pcap.count = 2
+    end
+
+    after(:each) do
+      @pcap.close
+    end
+
+    it_should_behave_like "Handler"
+
+    it "should provide statistics about packets received/dropped" do
+      @pcap.loop
+
+      stats = @pcap.stats
+      stats.received.should == 2
+    end
+  end
+
+  describe "dead" do
+    before(:each) do
+      @pcap = PCap.open_dead
+    end
+
+    after(:each) do
+      @pcap.close
+    end
+  end
+end
