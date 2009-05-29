8d7f8cc28212c77877ef5ff34565aede751608b3
postmodern <postmodern.mod3@gmail.com>
2009-05-29 13:14:00 -0700

    Prefix Ethernet types with _TYPE to avoid name conflicts.

--- lib/pcap/packets/ethernet.rb
+++ lib/pcap/packets/ethernet.rb
@@ -11,17 +11,17 @@ module FFI
 
         include Packet
 
-        PUP = 0x0200             # Xerox PUP
-        SPRITE = 0x0500          # Sprite
-        IP = 0x0800              # IP
-        ARP = 0x0806             # Address Resolution Protocol
-        RARP = 0x8035            # Reverse ARP
-        APPLE_TALK = 0x809b      # AppleTalk protocol
-        AARP = 0x80f3            # AppleTalk ARP
-        VLAN = 0x8100            # IEEE 802.1Q VLAN tagging
-        IPX = 0x8137             # IPX
-        IPV6 = 0x86dd            # IP protocol version 6
-        LOOPBACK = 0x9000        # used to test interfaces
+        PUP_TYPE = 0x0200             # Xerox PUP
+        SPRITE_TYPE = 0x0500          # Sprite
+        IP_TYPE = 0x0800              # IP
+        ARP_TYPE = 0x0806             # Address Resolution Protocol
+        RARP_TYPE = 0x8035            # Reverse ARP
+        APPLE_TALK_TYPE = 0x809b      # AppleTalk protocol
+        AARP_TYPE = 0x80f3            # AppleTalk ARP
+        VLAN_TYPE = 0x8100            # IEEE 802.1Q VLAN tagging
+        IPX_TYPE = 0x8137             # IPX
+        IPV6_TYPE = 0x86dd            # IP protocol version 6
+        LOOPBACK_TYPE = 0x9000        # used to test interfaces
 
         # Size of an Ethernet header
         SIZE = 14
@@ -45,47 +45,47 @@ module FFI
         end
 
         def is_pup?
-          self[:ether_type] == PUP
+          self[:ether_type] == PUP_TYPE
         end
 
         def is_sprite?
-          self[:ether_type] == PUP
+          self[:ether_type] == SPRITE_TYPE
         end
 
         def is_ip?
-          self[:ether_type] == IP
+          self[:ether_type] == IP_TYPE
         end
 
         def is_arp?
-          self[:ether_type] == ARP
+          self[:ether_type] == ARP_TYPE
         end
 
         def is_rarp?
-          self[:ether_type] == RARP
+          self[:ether_type] == RARP_TYPE
         end
 
         def is_apple_talk?
-          self[:ether_type] == APPLE_TALK
+          self[:ether_type] == APPLE_TALK_TYPE
         end
 
         def is_aarp?
-          self[:ether_type] == AARP
+          self[:ether_type] == AARP_TYPE
         end
 
         def is_vlan?
-          self[:ether_type] == VLAN
+          self[:ether_type] == VLAN_TYPE
         end
 
         def is_ipx?
-          self[:ether_type] == IPX
+          self[:ether_type] == IPX_TYPE
         end
 
         def is_ipv6?
-          self[:ether_type] == IPV6
+          self[:ether_type] == IPV6_TYPE
         end
 
         def is_loopback?
-          self[:ether_type] == IPV6
+          self[:ether_type] == IPV6_TYPE
         end
 
       end
