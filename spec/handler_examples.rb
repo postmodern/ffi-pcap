7740b195d80f446d5195058a607df4bba67b8085
postmodern <postmodern.mod3@gmail.com>
2009-05-28 18:29:24 -0700

    Added a spec for Handler#datalink.

--- spec/handler_examples.rb
+++ spec/handler_examples.rb
@@ -3,6 +3,13 @@ require 'pcap/handler'
 require 'spec_helper'
 
 shared_examples_for "Handler" do
+  it "must have a datalink" do
+    datalink = @pcap.datalink
+
+    datalink.value.should_not be_nil
+    datalink.name.should_not be_empty
+  end
+
   it "should pass packets to a callback" do
     @pcap.callback do |user,pkthdr,bytes|
       hdr = PacketHeader.new(pkthdr)
@@ -13,5 +20,16 @@ shared_examples_for "Handler" do
     end
 
     @pcap.loop
+  end
+
+  it "should be able to get the next packet" do
+    header, data = @pcap.next
+
+    header.should_not be_nil
+    header.captured.should_not == 0
+    header.length.should_not == 0
+
+    data.should_not be_nil
+    data.should_not be_null
   end
 end
