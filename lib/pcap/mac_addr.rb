1eef689d3b15ad8787bbde93fc0aa79c4e4ec0d6
postmodern <postmodern.mod3@gmail.com>
2009-05-29 12:58:39 -0700

    Forgot to call to_a on self[:bytes].

--- lib/pcap/mac_addr.rb
+++ lib/pcap/mac_addr.rb
@@ -12,7 +12,7 @@ module FFI
       # Converts the MAC address to hex form.
       #
       def to_s
-        "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % self[:bytes]
+        "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % self[:bytes].to_a
       end
 
     end
