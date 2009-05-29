042af71265de41cec7ef556c6fba24e058b59929
postmodern <postmodern.mod3@gmail.com>
2009-05-29 12:47:24 -0700

    Added Packet#next and Packet#prev.

--- lib/pcap/packet.rb
+++ lib/pcap/packet.rb
@@ -1,11 +1,35 @@
 module FFI
   module PCap
     module Packet
+      def self.included(base)
+        base.module_eval do
+          # previous packet in the payload
+          attr_reader :prev
+        end
+      end
+
+      #
+      # Creates a new packet from the specified _ptr_ and the
+      # given _prev_ packet.
+      #
+      def initialize(ptr,prev=nil)
+        super(ptr)
+
+        @prev = prev
+      end
+
       #
       # Returns the data payload of the packet.
       #
       def payload
         self + self.size
+      end
+
+      #
+      # Default method which returns the next packet in the payload.
+      #
+      def next
+        nil
       end
     end
   end
