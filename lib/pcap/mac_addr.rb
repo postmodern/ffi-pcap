b803c6ed5c67a99a2ff8b910f2dfba733a62f1c2
postmodern <postmodern.mod3@gmail.com>
2009-05-29 19:44:28 -0700

    Made MACAddr more usable.

--- lib/pcap/mac_addr.rb
+++ lib/pcap/mac_addr.rb
@@ -8,6 +8,14 @@ module FFI
 
       layout :bytes, [:uchar, SIZE]
 
+      def self.parse(str)
+        digits = str.split(':').map { |b| b.hex }
+        mac = self.new
+
+        mac[:bytes].to_ptr.put_array_of_uint8(0,digits[0,SIZE])
+        return mac
+      end
+
       #
       # Returns +true+ if the MAC is a broadcast address, returns +false+
       # otherwise.
@@ -20,11 +28,32 @@ module FFI
         return true
       end
 
+      def [](field)
+        if field.kind_of?(Integer)
+          return self.to_ptr.get_uint8(field)
+        else
+          return super(field)
+        end
+      end
+
+      def []=(field,value)
+        if field.kind_of?(Integer)
+          self.to_ptr.put_uint8(field,value.to_i)
+          return value
+        else
+          return super(field)
+        end
+      end
+
+      def to_a
+        self[:bytes].to_a
+      end
+
       #
       # Converts the MAC address to hex form.
       #
       def to_s
-        "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % self[:bytes].to_a
+        "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % self.to_a
       end
 
     end
