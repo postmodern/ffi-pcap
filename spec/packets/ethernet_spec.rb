9998c26b505f9a276ba42d4754c8ff37809d25fe
postmodern <postmodern.mod3@gmail.com>
2009-06-10 15:17:50 -0700

    DRY up the Packets::Ethernet specs a little.

--- spec/packets/ethernet_spec.rb
+++ spec/packets/ethernet_spec.rb
@@ -5,6 +5,14 @@ require 'spec_helper'
 require 'helpers/dumps'
 
 describe Packets::Ethernet do
+  before(:all) do
+    pcap = load_dump('http')
+    header, raw = pcap.next
+
+    @ether = raw.next
+    pcap.close
+  end
+
   before(:each) do
     @pcap = load_dump('http')
   end
@@ -26,46 +34,28 @@ describe Packets::Ethernet do
   end
 
   it "should be exactly 14 bytes long" do
-    header, raw = @pcap.next
-    ether = raw.next
-
-    ether.size.should == 14
+    @ether.size.should == 14
   end
 
   it "should have a source MAC address" do
-    header, raw = @pcap.next
-    ether = raw.next
-
-    ether.src_mac.should == [0x00, 0x23, 0x4e, 0x57, 0x7e, 0x51]
+    @ether.src_mac.should == [0x00, 0x23, 0x4e, 0x57, 0x7e, 0x51]
   end
 
   it "should have a destination MAC address" do
-    header, raw = @pcap.next
-    ether = raw.next
-
-    ether.dest_mac.should == [0x00, 0x16, 0x01, 0xed, 0x0d, 0x70]
-  end
-
-  it "should have an ethernet type" do
-    header, raw = @pcap.next
-    ether = raw.next
-
-    ether.type.should == Packets::Ethernet::IP_TYPE
+    @ether.dest_mac.should == [0x00, 0x16, 0x01, 0xed, 0x0d, 0x70]
   end
 
   it "should be able to determine payload type" do
-    header, raw = @pcap.next
-    ether = raw.next
+    @ether.should be_ip
+    @ether.should_not be_pup
+  end
 
-    ether.should be_ip
-    ether.should_not be_pup
+  it "should have an ethernet type" do
+    @ether.type.should == Packets::Ethernet::IP_TYPE
   end
 
   it "should have a payload" do
-    header, raw = @pcap.next
-    ether = raw.next
-
-    ether.payload.should_not be_nil
-    ether.payload.should_not be_null
+    @ether.payload.should_not be_nil
+    @ether.payload.should_not be_null
   end
 end
