212c11067cea41c1b8ac3c633878b8ca91719869
Eric Monti <emonti@matasano.com>
2010-01-03 11:36:32 -0600

    revert to jeweler

---
+++ tasks/rdoc.rb
@@ -0,0 +1,17 @@
+
+require 'rake/rdoctask'  
+
+Rake::RDocTask.new do |rdoc|  
+  if File.exist?('VERSION')  
+    version = File.read('VERSION')  
+  else  
+    version = ""  
+  end  
+  
+  rdoc.rdoc_dir = 'rdoc'  
+  rdoc.title = "ffi-pcap #{version}"  
+  rdoc.rdoc_files.include('README*')  
+  rdoc.rdoc_files.include('History*')  
+  rdoc.rdoc_files.include('lib/**/*.rb')  
+end 
+
