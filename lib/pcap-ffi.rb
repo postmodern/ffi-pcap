b4090a4c6a9a077796a6332a1cb17ca9c83aa179
Eric Monti <emonti@matasano.com>
2010-01-03 14:35:51 -0600

    tweak require order

--- lib/pcap-ffi.rb
+++ lib/pcap-ffi.rb
@@ -1,12 +1,12 @@
 begin
   require 'rubygems'
-  gem 'ffi_dry'
 rescue LoadError
 end
 
 require 'ffi/dry'
 require 'ffi/packets'
 
+require 'pcap-ffi/ffi'
 require 'pcap-ffi/if'
 require 'pcap-ffi/addr'
 require 'pcap-ffi/file_header'
@@ -15,5 +15,4 @@ require 'pcap-ffi/stat'
 require 'pcap-ffi/data_link'
 require 'pcap-ffi/handler'
 require 'pcap-ffi/version'
-require 'pcap-ffi/ffi'
 require 'pcap-ffi/pcap'
