35ba623c1b5697f0b7a71c3eb7174d1f0c92cd49
Eric Monti <emonti@matasano.com>
2010-01-04 15:21:53 -0600

    doc and desc

--- lib/pcap-ffi/addr.rb
+++ lib/pcap-ffi/addr.rb
@@ -1,18 +1,20 @@
-require 'pcap-ffi/typedefs'
-
-require 'ffi/struct'
+require 'pcap-ffi/bsd'
 
 module FFI
   module PCap
+
+    # Representation of an interface address.
+    #
+    # See pcap_addr struct in pcap.h
     class Addr < FFI::Struct
       include FFI::DRY::StructHelper
 
       dsl_layout do
         p_struct :next,       Addr
-        p_struct :addr,       SockAddr
-        p_struct :netmask,    SockAddr
-        p_struct :broadcast,  SockAddr
-        p_struct :dest_addr,  SockAddr
+        p_struct :addr,       SockAddr, :desc => 'address'
+        p_struct :netmask,    SockAddr, :desc => 'netmask of the address'
+        p_struct :broadcast,  SockAddr, :desc => 'broadcast for the address'
+        p_struct :dest_addr,  SockAddr, :desc => 'p2p destination for address'
       end
 
     end
