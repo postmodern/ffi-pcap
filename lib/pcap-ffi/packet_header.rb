3f712c419eeb53747be4812cfe602f57990198bf
Eric Monti <emonti@matasano.com>
2010-01-04 15:08:03 -0600

    add desc and doc

--- lib/pcap-ffi/packet_header.rb
+++ lib/pcap-ffi/packet_header.rb
@@ -2,13 +2,15 @@ require 'pcap-ffi/time_val'
 
 module FFI
   module PCap
+
+    # Generic per-packet information, as supplied by libpcap
     class PacketHeader < FFI::Struct
       include FFI::DRY::StructHelper
 
       dsl_layout do
-        struct :ts,      TimeVal
-        field  :caplen,  :bpf_uint32
-        field  :len,     :bpf_uint32
+        struct :ts,      TimeVal,     :desc => 'time stamp'
+        field  :caplen,  :bpf_uint32, :desc => 'length of portion present'
+        field  :len,     :bpf_uint32, :desc => 'length of packet (off wire)'
       end
 
       alias timestamp ts
@@ -16,5 +18,6 @@ module FFI
       alias length len
 
     end
+
   end
 end
