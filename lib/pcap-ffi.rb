9b918c23de699b6b68822b1e5197d18725722138
Eric Monti <emonti@matasano.com>
2010-01-11 17:08:35 -0600

    add exceptions require and organize reqs

--- lib/pcap-ffi.rb
+++ lib/pcap-ffi.rb
@@ -15,9 +15,10 @@ module FFI
 end
 
 require 'pcap-ffi/version'
+require 'pcap-ffi/exceptions'
 
+# FFI typedefs, pointer wrappers, and struct
 require 'pcap-ffi/typedefs'
-
 require 'pcap-ffi/bsd'
 require 'pcap-ffi/addr'
 require 'pcap-ffi/interface'
@@ -27,10 +28,10 @@ require 'pcap-ffi/packet_header'
 require 'pcap-ffi/stat'
 require 'pcap-ffi/bpf'
 require 'pcap-ffi/dumper'
+
+# Ruby FFI function bindings, sugar, and misc wrappers
 require 'pcap-ffi/error_buffer'
-
 require 'pcap-ffi/functions'
-
 require 'pcap-ffi/data_link'
 require 'pcap-ffi/packet'
 require 'pcap-ffi/handler'
