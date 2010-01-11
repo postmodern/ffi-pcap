6e5332785b09924d1bef3891771d8b297d878587
Eric Monti <emonti@matasano.com>
2010-01-10 23:25:05 -0600

    oved old packet stuff aside

--- lib/pcap-ffi/time_val.rb
+++ lib/pcap-ffi/time_val.rb
@@ -8,16 +8,40 @@ module FFI
         field :tv_usec, :suseconds_t
       end
 
+      def initialize(*args)
+        if args.size == 1 and (t=args[0]).kind_of?(Time)
+          self.time = t
+        else
+          super(*args)
+        end
+      end
+
       alias sec tv_sec
       alias usec tv_usec
 
-      def to_time
-        Time.at(self[:tv_sec], self[:tv_usec])
+      # Returns the time value as a ruby Time object.
+      #
+      # @return [Time]
+      #   A ruby time object derived from this TimeVal.
+      def time
+        Time.at(self.tv_sec, self.tv_usec)
       end
 
-#      def to_s
-#        to_time.to_s
-#      end
+      alias to_time time
+
+      # Sets the time value from a ruby Time object
+      #
+      # @param [Time] t
+      #   A ruby time object from which to set the time.
+      #
+      # @return [Time]
+      #   Returns the same Time object supplied per convention.
+      #
+      def time=(t)
+        self.tv_sec = t.tv_sec
+        self.tv_usec = t.tv_usec
+        return t
+      end
 
     end
   end
