20f63c93d5db3943c0b10a666697287daf9ae79f
Eric Monti <emonti@matasano.com>
2010-01-14 20:28:48 -0600

    nested Live injection spec

--- spec/injection_spec.rb
+++ spec/injection_spec.rb
@@ -1,43 +1,44 @@
 require 'spec_helper'
 
-describe "PCap::Live packet injection" do
-  before(:all) do
-    @pcap = PCap.open_live :device => PCAP_DEV, 
-                           :promisc => false,
-                           :timeout => 100,
-                           :snaplen => 8192
-  end
+describe PCap::Live do
+  describe "packet injection" do
+    before(:all) do
+      @pcap = PCap.open_live :device => PCAP_DEV, 
+                             :promisc => false,
+                             :timeout => 100,
+                             :snaplen => 8192
+    end
 
-  after(:all) do
-    @pcap.close
-  end
+    after(:all) do
+      @pcap.close
+    end
 
-  it "should detect when an invalid argument is supplied" do
-    lambda { @pcap.inject(Object.new)}.should raise_error(ArgumentError)
-    lambda { @pcap.inject(nil)}.should raise_error(ArgumentError)
-    lambda { @pcap.inject(1)}.should raise_error(ArgumentError)
-    lambda { @pcap.inject([])}.should raise_error(ArgumentError)
-    lambda { @pcap.inject(:foo => :bar)}.should raise_error(ArgumentError)
-    lambda { 
-      @pcap.inject(FFI::MemoryPointer.new(10))
-    }.should raise_error(ArgumentError)
-  end
+    it "should detect when an invalid argument is supplied" do
+      lambda { @pcap.inject(Object.new)}.should raise_error(ArgumentError)
+      lambda { @pcap.inject(nil)}.should raise_error(ArgumentError)
+      lambda { @pcap.inject(1)}.should raise_error(ArgumentError)
+      lambda { @pcap.inject([])}.should raise_error(ArgumentError)
+      lambda { @pcap.inject(:foo => :bar)}.should raise_error(ArgumentError)
+      lambda { 
+        @pcap.inject(FFI::MemoryPointer.new(10))
+      }.should raise_error(ArgumentError)
+    end
 
-  it "should allow injection of a Strings using inject()" do
-    test_data = "A" * 1024
-    @pcap.inject(test_data).should == test_data.size
-    p=@pcap.next()
-    Packet.should === p
-    p.body.should == test_data
-  end
+    it "should allow injection of a Strings using inject()" do
+      test_data = "A" * 1024
+      @pcap.inject(test_data).should == test_data.size
+      p=@pcap.next()
+      Packet.should === p
+      p.body.should == test_data
+    end
 
-  it "should allow injection of a Packet using inject()" do
-    test_data = "A" * 512
-    @pcap.inject(Packet.from_string(test_data)).should == test_data.size
-    p=@pcap.next()
-    Packet.should === p
-    p.body.should == test_data
-  end
+    it "should allow injection of a Packet using inject()" do
+      test_data = "A" * 512
+      @pcap.inject(Packet.from_string(test_data)).should == test_data.size
+      p=@pcap.next()
+      Packet.should === p
+      p.body.should == test_data
+    end
 
+  end
 end
-
