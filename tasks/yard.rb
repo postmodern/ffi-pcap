ee9591351b428b54bbee60ffd6d4fcf12dffcc54
Eric Monti <emonti@matasano.com>
2010-01-14 21:06:28 -0600

    recover from missing yard

--- tasks/yard.rb
+++ tasks/yard.rb
@@ -1,12 +1,16 @@
-require 'yard'
+begin
+  require 'yard'
 
-YARD::Rake::YardocTask.new do |t|
-  t.files   = ['lib/**/*.rb']
-  t.options = [
-    '--protected',
-    '--files', 'History.rdoc',
-    '--title', 'ffi-pcap'
-  ]
+  YARD::Rake::YardocTask.new do |t|
+    t.files   = ['lib/**/*.rb']
+    t.options = [
+      '--protected',
+      '--files', 'History.rdoc',
+      '--title', 'ffi-pcap'
+    ]
+  end
+
+  task :docs => :yard
+rescue LoadError
 end
 
-task :docs => :yard
