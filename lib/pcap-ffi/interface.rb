17197ac55f9bfcb1963c06add4b96919a2295258
Eric Monti <emonti@matasano.com>
2010-01-15 22:31:27 -0600

    cruft

--- lib/pcap-ffi/interface.rb
+++ lib/pcap-ffi/interface.rb
@@ -16,16 +16,12 @@ module FFI
         field     :name,        :string, :desc => 'name used by pcap_open_live()'
         field     :description, :string, :desc => 'text description, or NULL'
         p_struct  :addresses,   Addr,    :desc => 'address linked list'
-        field     :flags,       :bpf_uint32, :desc => 'PCAP_IF_ interface flags'
+        field     :flags,   :bpf_uint32, :desc => 'PCAP_IF_ interface flags'
       end
 
       def loopback?
         self.flags & LOOPBACK == LOOPBACK
       end
-
-#      def to_s
-#        self[:name]
-#      end
 
     end
 
