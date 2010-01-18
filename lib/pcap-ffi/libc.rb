7ef93b7035279d230f93c2b90b547aa83a03e840
Eric Monti <emonti@matasano.com>
2010-01-18 11:43:48 -0600

    win32 needs a :size_t typedef

--- lib/pcap-ffi/libc.rb
+++ lib/pcap-ffi/libc.rb
@@ -4,7 +4,13 @@ module FFI
     module CRT
       extend FFI::Library
 
-      begin; ffi_lib 'msvcrt'; rescue LoadError; end
+      begin
+        ffi_lib 'msvcrt'
+      rescue LoadError
+      end
+
+      typedef :ulong, :size_t  # not all platforms have this set for FFI
+
       attach_function :free, [:pointer], :void
       attach_function :memcpy, [:pointer, :pointer, :size_t], :pointer
     end
