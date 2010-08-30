733ea3457bd253d0b1b1d94c83727510c38e75c6
Eric Monti <esmonti@gmail.com>
2010-08-30 13:24:49 -0400

    added a test example for an eventmachine driven pcap handler using  a timer instead of selectable fd

---
+++ examples/em_timer.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+require 'ffi/pcap'
+require 'eventmachine'
+
+dev = ARGV.shift || 'lo0'
+if ARGV[0]
+  filter = ARGV.join(' ')
+end
+
+EM.run{
+  pcap = FFI::PCap::Live.new(:device => dev, :timeout => 1)
+  pcap.setfilter filter if filter
+
+  timer = EM::PeriodicTimer.new(0.0001) do
+    puts "Dispatch!" if $DEBUG
+    pcap.dispatch(:count => -1) do |this, pkt|
+      puts "#{pkt.time}:"
+      puts pkt.body.bytes.to_a.map{|c|  "%0.2x" % c }.join(" ")
+    end
+    puts "end dispatch" if $DEBUG
+  end
+}
+
