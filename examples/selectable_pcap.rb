b9759097221e24545b152bb66ac9240fe2d24138
Eric Monti <esmonti@gmail.com>
2010-08-30 13:24:20 -0400

    added examples for trying out experimental selectable pcap with and without eventmachine

--- examples/selectable_pcap.rb
+++ examples/selectable_pcap.rb
@@ -3,33 +3,24 @@
 require 'ffi/pcap'
 require 'eventmachine'
 
-class PcapWatcher < EM::Connection
-  def initialize(pcap)
-    @pcap = pcap
-  end
-
-  def notify_readable(*args)
-    puts "Dispatch!"
-    @pcap.dispatch() do |this, pkt|
-      puts "#{pkt.time}:"
-      puts pkt.body.bytes.to_a.map{|c|  "%0.2x" % c }.join(" ")
-    end
-    puts "end dispatch"
-  end
-
-end
-
 dev = ARGV.shift || 'lo0'
 if ARGV[0]
   filter = ARGV.join(' ')
 end
 
 EM.run{
-  pcap = FFI::PCap::Live.new(:device => dev, :timeout => 100)
+  pcap = FFI::PCap::Live.new(:device => dev, :timeout => 1)
   pcap.nonblocking=true
   pcap.setfilter filter if filter
 
-  conn = EM.watch pcap.selectable_fd, PcapWatcher, pcap
-  conn.notify_readable = true
+  fd = pcap.fileno
+  io=IO.new(fd)
+  while Kernel.select([io],nil,nil)
+    p :meep
+    pcap.dispatch() do |this,pkt| 
+      puts pkt.time
+      puts pkt.body.bytes.map{|x| "%0.2x" % x}.join(' ')
+    end
+  end
 }
 
